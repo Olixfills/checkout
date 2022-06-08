78bcd1e5d48e5fbcbf150b1ccaf6248a97c8bd5b
Oni Olayiwola <shasha@pop-os.localdomain>
2022-06-08 14:59:20 +0100

    header page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Header from "./components/Header";
 import Card from "./components/Card";
 import Total from "./components/Total";
 import Checkout from "./components/Checkout";
+import { Container, Divider } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Card />
-      <Total />
-      <Checkout />
+      <Container>
+        <Divider light />
+        <Card />
+        <Total />
+        <Checkout />
+      </Container>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Avatar, Stack, Typography } from "@mui/material";
 
+const pages = ["TRIPS", "RECENTLY VIEWED", "BOOKINGS"];
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Stack
+        direction="row"
+        justifyContent="end"
+        alignItems="center"
+        gap={8}
+        px={8}
+        paddingTop={4}
+        paddingBottom={10}
+      >
+        {pages.map((page) => (
+          <Typography
+            variant="subtitle1"
+            textAlign="center"
+            sx={{ color: "#484848", fontWeight: "500" }}
+          >
+            {page}
+          </Typography>
+        ))}
+        <Avatar
+          alt="image"
+          src="https://i3.wp.com/elcomercio.pe/resizer/5OAnUCe5uU-lMrEY4JKDBbXEhvk=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/6LOMSVPYQ5GVVOEC6XRE5GGXEA.jpg?resize=1024,1024"
+        />
+      </Stack>
+    </header>
+  );
 };
 
 export default Header;
