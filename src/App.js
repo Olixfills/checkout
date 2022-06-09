d39cde4490d4ccf735e12243ebae5d4453d58b31
Oni Olayiwola <shasha@pop-os.localdomain>
2022-06-09 14:00:30 +0100

    total comp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import Header from "./components/Header";
 import Card from "./components/Card";
 import Total from "./components/Total";
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Box, Divider, Stack, Typography } from "@mui/material";
+
+const Tot = ({ head, amount, sub }) => {
+  return (
+    <Stack direction="row" justifyContent="space-between">
+      <Box display="flex" alignItems="center">
+        <Typography variant="subtitle2" sx={{ fontWeight: "600" }}>
+          {head}
+        </Typography>
+        <Typography sx={{ color: "rgba(0, 0, 0, 0.3)" }}>{sub}</Typography>
+      </Box>
+      <Typography variant="subtitle1" sx={{ fontWeight: "600" }}>
+        {amount}
+      </Typography>
+    </Stack>
+  );
+};
 
 const Total = () => {
-  return <div>Total</div>;
+  return (
+    <Stack gap={2}>
+      <Divider />
+      <Tot head="Subtotal" amount="₦2,497.00" />
+      <Tot head="Estimated TAX" amount="₦119.64" />
+      <Tot head="Promotional Code:" sub="Z4KXLM9A" amount="₦-60.00" />
+      <Divider sx={{ py: 1 }} />
+    </Stack>
+  );
 };
 
 export default Total;
