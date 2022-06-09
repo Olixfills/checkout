24da79f34bd557a26fe625a4eabc4da4d5bcb1ab
Oni Olayiwola <shasha@pop-os.localdomain>
2022-06-09 14:17:33 +0100

    complete

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,7 +1,22 @@
+import { Button, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      py={4}
+      marginBottom={4}
+    >
+      <Button variant="contained" px={2}>
+        Complete payment
+      </Button>
+      <Typography variant="h6" sx={{ fontWeight: "600" }}>
+        TOTAL: ₦2556.64
+      </Typography>
+    </Stack>
+  );
 };
 
 export default Checkout;
